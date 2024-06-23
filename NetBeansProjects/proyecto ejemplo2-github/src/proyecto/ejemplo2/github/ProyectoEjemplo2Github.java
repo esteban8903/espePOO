ddc2ce4e6957892955facba7455ea7cfc7c8762b
esteban8903 <andresesteban2403@gmail.com>
2024-06-22 22:17:50 -0500

    puse hola

---
+++ NetBeansProjects/proyecto ejemplo2-github/src/proyecto/ejemplo2/github/ProyectoEjemplo2Github.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.ejemplo2.github;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoEjemplo2Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+hola
